b42300ee3f1ac014df3f5f0206113b6ecd5db37b
= <=>
2020-06-20 03:42:15 +0500

    mern app

---
+++ client/src/components/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Loader = () => (
+	 <div className="progress" style={{display: 'flex', justifyContent: 'center', marginTop: '25px' }}>
+    	  <div className="indeterminate"></div>
+  	</div>
+)
+
